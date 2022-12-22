6f3bacde269828d202f0cb6ae1b19641c98d20c6
Patrick McLaughlin <patrickmc21@gmail.com>
2022-12-22 11:54:00 -0700

    Add jsdoc configuration, add to logger service

--- src/Services/Logger/index.ts
+++ src/Services/Logger/index.ts
@@ -1,5 +1,17 @@
-import * as winston from "winston";
+import * as winston from 'winston';
 
+/**
+ * Logger
+ *
+ * @class Logger
+ *
+ * @example
+ * const logger = new Logger();
+ * logger.info("your log message");
+ *
+ * @see https://github.com/winstonjs/winston
+ *
+ */
 class Logger {
   public loggingUtility: winston.Logger;
 
@@ -13,25 +25,44 @@ class Logger {
               colors: {
                 info: 'blue',
                 warn: 'yellow',
-                error: 'bold red'
-              }
+                error: 'bold red',
+              },
             }),
             winston.format.timestamp(),
-            winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`),
-          )
-        })
-      ]
+            winston.format.printf((info) => `${info.timestamp} [${info.level}]: ${info.message}`)
+          ),
+        }),
+      ],
     });
   }
 
+  /**
+   * @memberof Logger
+   * @method info
+   * @description Logs a message to the console with severity "info"
+   * @param message {String} message to log
+   */
   public info(message: string): void {
     this.loggingUtility.info(message);
   }
 
+  /**
+   * @memberof Logger
+   * @method warn
+   * @description Logs a message to the console with severity "warn"
+   * @param message {String} message to log
+   */
   public warn(message: string): void {
     this.loggingUtility.warn(message);
   }
 
+  /**
+   * @memberof Logger
+   * @method error
+   * @description Logs a message to the console with severity "error"
+   * @param message {String} message to log
+   * @param [meta] {any} Additional metadata to attach to the log
+   */
   public error(message: string, meta?: unknown): void {
     this.loggingUtility.error(message, meta);
   }
