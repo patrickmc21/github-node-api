14d5790da00b9eb231e9e6efae67f29545b60a30
Patrick McLaughlin <patrickmc21@gmail.com>
2022-12-22 16:16:36 -0700

    Add integration tests for GitHubDal

--- src/Components/GitHub/github.dal.ts
+++ src/Components/GitHub/github.dal.ts
@@ -1,18 +1,21 @@
 import HttpDal from '../../AbstractClasses/httpDal';
-import { GitHubPullRequest } from '../../Types/GitHubApi.type';
+import { GitHubPullRequest, GitHubRepo, GitHubUser } from '../../Types/GitHubApi.type';
 
 export default class GithubDal extends HttpDal {
   private static GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  private static BASE_GITHUB_HEADERS = {
+    Accept: 'application/vnd.github+json',
+    Authorization: `Bearer ${GithubDal.GITHUB_TOKEN}`,
+    'X-GitHub-Api-Version': '2022-11-28',
+  };
   constructor(baseUrl: string) {
     super(baseUrl);
   }
 
-  public async getPullRequestsByRepo(owner: string, repo: string): Promise<GitHubPullRequest[]> {
-    const url = `/repos/${owner}/${repo}/pulls`;
+  public async getUser(username: string): Promise<GitHubUser> {
+    const url = `/users/${username}`;
     const headers = {
-      Accept: 'application/vnd.github+json',
-      Authorization: `Bearer ${GithubDal.GITHUB_TOKEN}`,
-      'X-GitHub-Api-Version': '2022-11-28',
+      ...GithubDal.BASE_GITHUB_HEADERS,
     };
 
     const response = await this.get(url, headers);
@@ -21,7 +24,59 @@ export default class GithubDal extends HttpDal {
       return data;
     } else {
       let message = 'GitHub API Error:';
-      switch (response.statusCode) {
+      switch (response.status) {
+        case 404:
+          message += ' Resource not found';
+          break;
+        default:
+          message += ' An unexpected error has occured. Please try your request at a later time';
+      }
+      throw new Error(message);
+    }
+  }
+
+  public async getRepo(owner: string, repo: string): Promise<GitHubRepo> {
+    const url = `/repos/${owner}/${repo}`;
+    const headers = {
+      ...GithubDal.BASE_GITHUB_HEADERS,
+    };
+
+    const response = await this.get(url, headers);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      let message = 'GitHub API Error:';
+      switch (response.status) {
+        case 301:
+          message += ' Moved permanently';
+          break;
+        case 403:
+          message += ' Forbidden';
+          break;
+        case 404:
+          message += ' Resource not found';
+          break;
+        default:
+          message += ' An unexpected error has occured. Please try your request at a later time';
+      }
+      throw new Error(message);
+    }
+  }
+
+  public async getPullRequestsByRepo(owner: string, repo: string): Promise<GitHubPullRequest[]> {
+    const url = `/repos/${owner}/${repo}/pulls`;
+    const headers = {
+      ...GithubDal.BASE_GITHUB_HEADERS,
+    };
+
+    const response = await this.get(url, headers);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      let message = 'GitHub API Error:';
+      switch (response.status) {
         case 302:
           message += ' Not Modified';
           break;
--- src/Components/GitHub/tests/github.dal.integration.test.ts
+++ src/Components/GitHub/tests/github.dal.integration.test.ts
@@ -9,6 +9,52 @@ describe('GitHub Dal | Integration tests', () => {
     dal = gitHubAssembler.getDal();
   });
 
+  describe('#getUser()', () => {
+    let username: string;
+
+    beforeEach(() => {
+      username = 'patrickmc21';
+    });
+
+    it('Returns the user by provided username', async () => {
+      const result = await dal.getUser(username);
+      expect(result).toBeInstanceOf(Object);
+    });
+
+    it('Returns an error if provided username does not exist', async () => {
+      username = 'fake-owner';
+      try {
+        const result = await dal.getUser(username);
+      } catch (error) {
+        expect(error.message).toEqual('GitHub API Error: Resource not found');
+      }
+    });
+  });
+
+  describe('#getRepo()', () => {
+    let owner: string;
+    let repo: string;
+
+    beforeEach(() => {
+      owner = 'patrickmc21';
+      repo = 'github-node-api';
+    });
+
+    it('Returns all of the open PRs for a given owner and repo', async () => {
+      const result = await dal.getRepo(owner, repo);
+      expect(result).toBeInstanceOf(Object);
+    });
+
+    it('Returns an error if an invalid owner or repo is provided', async () => {
+      repo = 'fake-repo';
+      try {
+        const result = await dal.getRepo(owner, repo);
+      } catch (error) {
+        expect(error.message).toEqual('GitHub API Error: Resource not found');
+      }
+    });
+  });
+
   describe('#getPullRequestsByRepo()', () => {
     let owner: string;
     let repo: string;
