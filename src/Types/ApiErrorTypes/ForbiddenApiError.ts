1aadb53095232353c44bf9790eda02aeea904d08
Patrick McLaughlin <patrickmc21@gmail.com>
2022-12-26 15:10:13 -0700

    Add index to ApiError types directory

--- src/Types/ApiErrorTypes/ForbiddenApiError.ts
+++ src/Types/ApiErrorTypes/ForbiddenApiError.ts
@@ -1,12 +1,12 @@
 import BaseApiError from './BaseApiError';
 
-class Forbidden extends BaseApiError {
+class ForbiddenApiError extends BaseApiError {
   private static STATUS_CODE = 403;
   private static BASE_MESSAGE = 'Error: Forbidden';
 
   constructor(meta?: any) {
-    super(Forbidden.BASE_MESSAGE, Forbidden.STATUS_CODE, meta);
+    super(ForbiddenApiError.BASE_MESSAGE, ForbiddenApiError.STATUS_CODE, meta);
   }
 }
 
-export default Forbidden;
+export default ForbiddenApiError;
