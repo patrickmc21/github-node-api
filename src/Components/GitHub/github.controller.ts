26fa113c6170bbcbcd63b126d8fa61c3694ad3c0
Patrick McLaughlin <patrickmc21@gmail.com>
2022-12-27 11:14:03 -0700

    Add jsdoc to GitHubController

--- src/Components/GitHub/github.controller.ts
+++ src/Components/GitHub/github.controller.ts
@@ -3,12 +3,40 @@ import { BaseApiError, ExternalApiError, InternalApiError, NotFoundApiError } fr
 import { GitHubPullRequestFormatted } from '../../Types/GitHubPullRequest.type';
 import GitHubFactory from './github.factory';
 
+/**
+ * GitHubController
+ *
+ * @class GitHubController
+ *
+ * @extends Controller
+ *
+ * @example
+ * const gitHubController = new GitHubController(gitHubFactory);
+ * gitHubController.validateUser('myUserName');
+ *
+ * @description Controller layer for GitHub REST API component
+ */
 class GitHubController extends Controller {
+  /**
+   * @memberof GitHubController
+   * @protected
+   * @property {GitHubFactory} factory factory layer for controller
+   */
   protected factory: GitHubFactory;
+
   constructor(factory: GitHubFactory) {
     super(factory);
   }
 
+  /**
+   * @memberof GitHubController
+   * @async
+   * @method validateUser
+   * @description Validates a given user name.
+   * @param {string} user username to validate
+   * @returns {Promise<true>} Returns true if user exists in GitHub
+   * @throws {NotFoundApiError}
+   */
   public async validateUser(user: string): Promise<true> {
     try {
       await this.factory.getUser(user);
@@ -22,6 +50,16 @@ class GitHubController extends Controller {
     }
   }
 
+  /**
+   * @memberof GitHubController
+   * @async
+   * @method validateRepo
+   * @description Validates a given repo exists by name
+   * @param {string} owner github user who owns the repository
+   * @param {string} repo name of the repository
+   * @returns {Promise<true>} Returns true if repo exists in GitHub
+   * @throws {NotFoundApiError}
+   */
   public async validateRepo(owner: string, repo: string): Promise<true> {
     try {
       await this.factory.getRepo(owner, repo);
@@ -35,6 +73,16 @@ class GitHubController extends Controller {
     }
   }
 
+  /**
+   * @memberof GitHubController
+   * @async
+   * @method getOpenPullRequestsByRepo
+   * @description Gets all open pull requests associated with a github repo
+   * @param {string} owner github user who owns the repository
+   * @param {string} repo name of the github repository
+   * @returns {Promise<GitHubPullRequestFormatted[]>} Array of pull requests associated with a repo
+   * @throws {NotFoundApiError | ExternalApiError | InternalApiError}
+   */
   public async getOpenPullRequestsByRepo(owner: string, repo: string): Promise<GitHubPullRequestFormatted[]> {
     try {
       await Promise.all([this.validateUser(owner), this.validateRepo(owner, repo)]);
